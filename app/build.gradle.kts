bbc451ed9cac7f0d50134dffdb109fc705a7fb5c
osphvdhwj <150425690+osphvdhwj@users.noreply.github.com>
2026-03-14 09:56:50 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Coil for Icons
     implementation("io.coil-kt:coil-compose:2.5.0")
